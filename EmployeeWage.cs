8fa9eadd6906d93cd102d426c7d8f47b49dbac3d
saurabhs <saurabhpshirsath@gmail.com>
2021-07-24 23:02:34 +0530

    [saurabh] Add; Employee Wage For Month With Working Days

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -11,17 +11,20 @@ namespace EmployeewageComputation
         const int IS_FULL_TIME = 2;
         const int EMP_WAGE_PER_HOUR = 20;
         const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
         //variables
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
         /// <summary>
-        /// This Method is Used to Check Employee Wage For Month
+        /// This Method is Used For find Employee Wage For Month With Working Hrs Or Day Reached 
         /// </summary>
         public void EmpMonthWage()
 
         {
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+
+                totalWorkingDays++; 
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -36,10 +39,10 @@ namespace EmployeewageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_WAGE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("days:" + totalWorkingDays + " Emp Hrs :" + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
 
         }
