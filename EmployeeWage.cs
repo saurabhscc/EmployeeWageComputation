54f941bf4869696aed9f80d457907ddc3f363c8a
saurabhs <saurabhpshirsath@gmail.com>
2021-07-24 22:40:44 +0530

    [saurabh] Add; Check for Employee Wage For Month

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -10,33 +10,40 @@ namespace EmployeewageComputation
         const int IS_PART_TIME = 1;
         const int IS_FULL_TIME = 2;
         const int EMP_WAGE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
         //variables
-        int empHrs = 0;
-        int empWage = 0;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
+
         /// <summary>
-        /// This Method is Used to Check Employee Part Time Employee And Wage using SwitchCase
+        /// This Method is Used to Check Employee Wage For Month
         /// </summary>
-        public void PartTimeEmp()
-                  
+        public void EmpMonthWage()
+
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            empWage = empHrs * EMP_WAGE_PER_HOUR;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_WAGE_PER_HOUR;
+                totalEmpWage += empWage;
                 Console.WriteLine("Emp wage : " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
 
         }
         
     }
 }
+
